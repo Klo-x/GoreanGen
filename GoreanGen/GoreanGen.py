af666cbc51679bf9f4d35cc1a6e9c0e22b6339e1
If0n <80531148+If-0n@users.noreply.github.com>
2023-11-28 19:11:42 -0300

    Add files via upload

--- GoreanGen/GoreanGen.py
+++ GoreanGen/GoreanGen.py
@@ -1,92 +1,104 @@
 import os
-from turtle import color, title
 import subprocess
-from subprocess import call
-from colorama import Fore, Back, Style
+from colorama import Fore
 import requests
 import random
+import threading
+from rgbprint import *
 
-def banner():
-        print(f" ")
-        print(f" ")
-        print(Fore.MAGENTA+ f" $$$$$$\                                                     $$$$$$\                      ")
-        print(Fore.MAGENTA+ f"$$  __$$\                                                   $$  __$$\                     ")
-        print(Fore.MAGENTA+ f"$$ /  \__| $$$$$$\   $$$$$$\   $$$$$$\   $$$$$$\  $$$$$$$\  $$ /  \__| $$$$$$\  $$$$$$$\  ")
-        print(Fore.MAGENTA+ f"$$ |$$$$\ $$  __$$\ $$  __$$\ $$  __$$\  \____$$\ $$  __$$\ $$ |$$$$\ $$  __$$\ $$  __$$\ ")
-        print(Fore.MAGENTA+ f"$$ |\_$$ |$$ /  $$ |$$ |  \__|$$$$$$$$ | $$$$$$$ |$$ |  $$ |$$ |\_$$ |$$$$$$$$ |$$ |  $$ |")
-        print(Fore.MAGENTA+ f"$$ |  $$ |$$ |  $$ |$$ |      $$   ____|$$  __$$ |$$ |  $$ |$$ |  $$ |$$   ____|$$ |  $$ |")
-        print(Fore.MAGENTA+ f"\$$$$$$  |\$$$$$$  |$$ |      \$$$$$$$\ \$$$$$$$ |$$ |  $$ |\$$$$$$  |\$$$$$$$\ $$ |  $$ | ")
-        print(Fore.MAGENTA+ f" \______/  \______/ \__|       \_______| \_______|\__|  \__| \______/  \_______|\__|  \__| 1.4")
-        print(f" ")
-        print(f" ")
-banner()
+banner = f'''
+      ________                                     ________               
+     /  _____/  ___________   ____ _____    ____  /  _____/  ____   ____  
+    /   \  ___ /  _ \_  __ \_/ __ \\__   \  /    \/   \  ____/ __ \ /    \\ 
+    \    \_\  (  <_> )  | \/\  ___/ / __ \|   |  \    \_\  \  ___/|   |  \\ 
+     \______  /\____/|__|    \___  >____  /___|  /\______  /\___  >___|  /
+            \/                   \/     \/     \/        \/     \/     \/  v1.4 | Made By If0n
+'''
 
-while True:
-    genorcheck = input(f"{Fore.CYAN}> Generate or Check? (g/c): {Fore.RED}")
+def generate():
+    lowercase = "abcdefghijklmnopqrstuvwxyz"
+    uppercase = "ABCDEFGHIJKLMNOPQRSTUVWYYZ"
+    numbers = "0123456789"
 
-    def generate():
-        lowercase = "abcdefghijklmnopqrstuvwxyz"
-        uppercase = "ABCDEFGHIJKLMNOPQRSTUVWYYZ"
-        numbers = "0123456789"
+    amount = int(input(f"{Fore.CYAN}> How many to Generate ---( {Fore.RED}"))
+    upper, lower, nums = True, True, True
+    all_chars = ""
 
-        amount = int(input(f"{Fore.CYAN}> How many to Generate: {Fore.RED}"))
-        upper, lower, nums = True, True, True
-        all = ""
+    if upper:
+        all_chars += uppercase
+    if lower:
+        all_chars += lowercase
+    if nums:
+        all_chars += numbers
 
-        if upper:
-            all += uppercase
-        if lower:
-            all += lowercase
-        if nums:
-            all += numbers
+    count = 0
+    length = 16
 
-            count = 0
-            length = 16
-            n = open('nitros.txt', 'w+')
-            for x in range(amount):
-                code = "".join(random.sample(all,length))
-                n.write(code + "\n")
-                count += 1
-                print(f"{Fore.RED}{code}{Fore.CYAN} | Code Written ({Fore.RED}nitros.txt{Fore.CYAN}){Fore.RED} - x{count}{Fore.CYAN}/{Fore.RED}{amount}{Fore.CYAN}")
+    n = open('nitros.txt', 'w+')
+    for x in range(amount):
+        code = "".join(random.sample(all_chars, length))
+        n.write(code + "\n")
+        count += 1
+        print(f"{Fore.RED}{code}{Fore.CYAN} | Code Written ({Fore.RED}nitros.txt{Fore.CYAN}){Fore.RED} - x{count}{Fore.CYAN}/{Fore.RED}{amount}{Fore.CYAN}")
+    n.close()
 
-    def check():
-        with open("nitros.txt", "r") as nitrofile:
-            nitros = nitrofile.read().split("\n")
-        nitrofile.close()
-        with open("proxies.txt", "r") as proxyfile:
-            proxies = proxyfile.read().split("\n")
-        proxyfile.close()
-        count = 0
-        for i in range(len(nitros)):
-            nitro = nitros[i]
-            proxy = proxies[i]
-            ProxyParam = {"http://": proxy, "https://": proxy}
-            checkurl = requests.get(f"https://discordapp.com/api/v6/entitlements/gift-codes/{nitro}", proxies=ProxyParam, timeout=3)
-            if checkurl.status_code == 200:
-                count+=1
-                print(f"{Fore.CYAN}[+] {Fore.GREEN}{nitro}{Fore.CYAN} | Valid Code{Fore.RED} - x{count}{Fore.CYAN}/{Fore.RED}{len(nitros)-1}")
-            else:
-                count+=1
-                print(f"{Fore.CYAN}[+] {Fore.RED}{nitro}{Fore.CYAN} | Invalid Code{Fore.RED} - x{count}{Fore.CYAN}/{Fore.RED}{len(nitros)-1}")
+def check(nitros, proxies, index):
+    nitro = nitros[index]
+    proxy = random.choice(proxies)  # Randomly select a proxy for each nitro
+    ProxyParam = {"http://": proxy, "https://": proxy}
+    checkurl = requests.get(f"https://discordapp.com/api/v6/entitlements/gift-codes/{nitro}", proxies=ProxyParam, timeout=3)
 
-    def scrapeproxies():
-        input(f"{Fore.CYAN}> Press ENTER To Scrape HTTP Proxies")
-        url = requests.get("https://api.openproxylist.xyz/http.txt")
-        urlresult = url.text
-        linecount = 0
-        with open('proxies.txt', 'w') as proxyscrapelist:
-            proxyscrapelist.write(urlresult)
-            file = open('proxies.txt')
+    count = index + 1
+    if checkurl.status_code == 200:
+        print(f"{Fore.CYAN}[+] {Fore.GREEN}{nitro}{Fore.CYAN} | Valid Code{Fore.RED} - x{count}{Fore.CYAN}/{Fore.RED}{len(nitros)}")
+    else:
+        print(f"{Fore.CYAN}[+] {Fore.RED}{nitro}{Fore.CYAN} | Invalid Code{Fore.RED} - x{count}{Fore.CYAN}/{Fore.RED}{len(nitros)}")
+
+def scrape_proxies():
+    input(f"{Fore.CYAN}> Press ENTER To Scrape HTTP Proxies")
+    url = requests.get("https://api.openproxylist.xyz/http.txt")
+    url_result = url.text
+    line_count = 0
+    with open('proxies.txt', 'w') as proxy_scrape_list:
+        proxy_scrape_list.write(url_result)
+        with open('proxies.txt') as file:
             for line in file:
                 if line != "\n":
-                    linecount += 1
-            print(f"{Fore.CYAN}[+] Scraped {Fore.RED}{linecount}{Fore.CYAN} Proxies ({Fore.RED}proxies.txt{Fore.CYAN})")
-            file.close()
-            input(f"{Fore.CYAN}> Press ENTER To Start Checking")
+                    line_count += 1
+        print(f"{Fore.CYAN}[+] Scraped {Fore.RED}{line_count}{Fore.CYAN} Proxies ({Fore.RED}proxies.txt{Fore.CYAN})")
+        input(f"{Fore.CYAN}> Press ENTER To Start Checking")
 
-    if genorcheck == "g":
-        generate()
-    if genorcheck == "c":
-        scrapeproxies()
-        check()
+def main():
 
+    gradient_print(
+            f"{banner}\n",
+            start_color=Color.dark_blue,
+            end_color=Color.ghost_white
+    )
+
+    while True:
+        gen_or_check = input(f"{Fore.CYAN}> Generate or Check? (g/c)---( {Fore.RED}")
+
+        if gen_or_check == "g":
+            generate_thread = threading.Thread(target=generate)
+            generate_thread.start()
+            generate_thread.join()
+        elif gen_or_check == "c":
+            nitros = open("nitros.txt", "r").read().split("\n")[:-1]
+            proxies = open("proxies.txt", "r").read().split("\n")[:-1]
+
+            scrape_proxies_thread = threading.Thread(target=scrape_proxies)
+            scrape_proxies_thread.start()
+            scrape_proxies_thread.join()
+
+            check_threads = []
+            for i in range(len(nitros)):
+                thread = threading.Thread(target=check, args=(nitros, proxies, i))
+                check_threads.append(thread)
+                thread.start()
+
+            for thread in check_threads:
+                thread.join()
+
+if __name__ == "__main__":
+    main()
